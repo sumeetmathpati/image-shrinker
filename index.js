713af5f853cc95faaf80bb4b506f3cce75428a11
Sumeet Mathpati <sumeet.mathapti@gmail.com>
2021-01-10 11:51:22 +0000

    Created app

---
+++ index.js
@@ -0,0 +1,11 @@
+const {app, BrowserWindow} = require('electron')
+
+function createMainWindow() {
+    const mainWindow = new BrowserWindow({
+        title: 'Image Shrink',
+        width: 500,
+        height: 600
+    });
+}
+
+app.on('ready', createMainWindow);
